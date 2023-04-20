2e96d1cb06a367353aed0d9c7d730d742065880e
mohaliza138 <mohaliza138@gmail.com>
2023-04-21 03:41:43 +0430

    Validations: implemented register/login menu regexes

--- AP_FinalProject/src/main/java/model/enums/Validations.java
+++ AP_FinalProject/src/main/java/model/enums/Validations.java
@@ -1,4 +1,18 @@
 package model.enums;
 
+import java.util.regex.Pattern;
+
 public enum Validations {
+    VALID_USERNAME("[A-Za-z0-9_]+"),
+    STRONG_PASSWORD("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[^0-9a-zA-Z])\\S{6,}"),
+    VALID_EMAIL("[a-zA-Z0-9_\\.]+@[a-zA-Z0-9_\\.]+.[a-zA-Z0-9_\\.]+");
+    private final String regex;
+
+    Validations(String regex) {
+        this.regex = regex;
+    }
+
+    public static boolean check(String string, Validations validation) {
+        return Pattern.matches(validation.regex, string);
+    }
 }
