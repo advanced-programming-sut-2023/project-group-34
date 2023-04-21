05b0202e5ebc975cc871e97cb6b43a1f7bafb44b
mohaliza138 <mohaliza138@gmail.com>
2023-04-21 04:36:45 +0430

    Password: SHA-256 implemented

--- AP_FinalProject/src/main/java/model/user/Password.java
+++ AP_FinalProject/src/main/java/model/user/Password.java
@@ -2,13 +2,28 @@ package model.user;
 
 import model.enums.SecurityQuestion;
 
+import java.security.MessageDigest;
+
 public class Password {
     private String passwordName;
     private SecurityQuestion securityQuestion;
     private String answer;
 
-    private static String sha256Encrypt (String string) {
-        return null;
+    private static String sha256Encrypt (final String string) {
+        try{
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(string.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
     }
 
     public boolean checkPassword(String password) {
