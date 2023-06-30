e608177c9247a75416d25405f394ee3f7e643853
Mohammad Hossein Eslami <m20h030e@gmail.com>
2023-06-30 18:39:22 +0330

    started FriendShipMenu 2

--- AP_FinalProject/src/main/java/view/main/FriendshipMenuController.java
+++ AP_FinalProject/src/main/java/view/main/FriendshipMenuController.java
@@ -1,4 +1,20 @@
 package view.main;
 
-public class FriendshipMenuController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FriendshipMenuController implements Initializable {
+    @FXML
+    private TextField username;
+    @FXML
+    private Text usernameError;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
